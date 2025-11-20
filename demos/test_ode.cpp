85465483e65edd7a035f8800cf4510ad99d741f6
Natalia Tylek <e12332258@student.tuwien.ac.at>
2025-11-20 15:08:13 +0100

    after merge rk

--- demos/test_ode.cpp
+++ demos/test_ode.cpp
@@ -91,8 +91,8 @@ int main()
   // RungeKutta stepper(rhs, Gauss2a, Gauss2b, Gauss2c);
 
   // Gauss3c .. points tabulated, compute a,b:
-  auto [Gauss3a,Gauss3b] = ComputeABfromC (Gauss3c);
-  ImplicitRungeKutta stepper(rhs, Gauss3a, Gauss3b, Gauss3c);
+  // auto [Gauss3a,Gauss3b] = ComputeABfromC (Gauss3c);
+  // ImplicitRungeKutta stepper(rhs, Gauss3a, Gauss3b, Gauss3c);
 
 
   /*
